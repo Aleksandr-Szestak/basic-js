22992fe5dfc1f48cebf16d2fd290946f63845ba7
Aleksandr-Szestak <szestak@hotmail.com>
2023-12-20 11:39:55 +0300

    feat: add extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+//console.log(typeof str, typeof options);
+//--------------------------------------
+if (typeof str !== 'string'){
+  str = String(str);
+}
+
+if (options !== undefined &&
+  options.addition !== undefined &&
+  typeof options.addition !== 'string'){
+options.addition = String(options.addition);
+}
+//--------------------------------------
+let _separator, _additionSeparator;
+
+if (options !== undefined && options.separator !== undefined){
+  _separator = options.separator;
+} else {
+  _separator = '+';
+  }
+
+if (options !== undefined && options.additionSeparator !== undefined){
+  _additionSeparator = options.additionSeparator;
+} else {
+  _additionSeparator = '|';
+  }
+//--------------------------------------
+let _repeatTimes, _additionRepeatTimes
+
+if (options !== undefined && options.repeatTimes !== undefined){
+  _repeatTimes = options.repeatTimes;
+} else {
+  _repeatTimes = 1;
+  }
+
+if (options !== undefined && options.additionRepeatTimes !== undefined){
+  _additionRepeatTimes = options.additionRepeatTimes;
+} else {
+  _additionRepeatTimes = 1;
+  }
+//--------------------------------------
+let _addition = '';
+
+if (options !== undefined && options.addition !== undefined){
+  _addition += (options.addition + _additionSeparator) .repeat(_additionRepeatTimes - 1);
+  _addition += options.addition;
+}
+
+//console.log( _addition );
+//--------------------------------------
+let result = '';
+
+result += (str + _addition + _separator) .repeat(_repeatTimes - 1);
+result += str + _addition;
+
+return result;
 }
 
 module.exports = {
