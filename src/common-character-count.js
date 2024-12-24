2104baa4655265f1a6efedb33068ac9f2925526a
Aleksandr-Szestak <szestak@hotmail.com>
2024-12-24 13:49:23 +0300

    refactor: change formatting of code in common-character-count task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,22 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-  let i, j;
+  let i;
+  let j;
   let arrS2 = s2.split('');
   let result = 0;
-
-  for (i=0; i<s1.length; i++) {
-    for (j=0; j<arrS2.length; j++) {
-      if (arrS2[j] != null && s1[i] === arrS2[j]) {
+  for (i = 0; i < s1.length; i += 1) {
+    for (j = 0; j < arrS2.length; j += 1) {
+      if (arrS2[j] !== null && s1[i] === arrS2[j]) {
         result += 1;
         arrS2[j] = null;
         break;
       }
     }
   }
-return result;
+  return result;
 }
 
 module.exports = {
